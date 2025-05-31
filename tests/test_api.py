0e462ff9ac2635383ba9265d63eb7594c1afa330
Rafid Hossain <rafid_hossain@outlook.com>
2025-05-30 22:30:05 -0400

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,9 @@
-from fastapi.testclient import TestClient
-from src.api.fastapi_server import app
-
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from fastapi.testclient import TestClient
+from src.api.fastapi_server import app
 
 client = TestClient(app)
 
