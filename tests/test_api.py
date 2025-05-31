a9dda44be251ee25206d0ed7759314b1a9c721e2
Rafid Hossain <rafid_hossain@outlook.com>
2025-05-30 22:24:15 -0400

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,10 @@
 from fastapi.testclient import TestClient
 from src.api.fastapi_server import app
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 client = TestClient(app)
 
 def test_predict_price_endpoint():
